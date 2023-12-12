1bc9954579472f1dd65042d0777bc0d9e98b7420
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-12 00:27:48 +0000

    Mon, Dec 11, 2023, 7:27 PM -05:00

--- scores.c
+++ scores.c
@@ -2,10 +2,11 @@
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
+    int score[0] = 72;
+    int score[1] = 73;
+    int score[2]= 33;
 
     //print adverage
-    printf("Average %f\n", (score1 + score2 + score3));
+    printf("Average %f\n", (score[0] + score[1] + score[2] / 3.0));
 }
