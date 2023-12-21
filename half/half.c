eb0a2cde407e693da4f88b77fe4749c409cb8065
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-21 21:20:04 +0000

    Thu, Dec 21, 2023, 4:20 PM -05:00

--- half/half.c
+++ half/half.c
@@ -16,7 +16,7 @@ int main(void)
 }
 
 // TODO: Complete the function
-float half(float bill, float tax, int tip)
+    float half(float bill, float tax, int tip)
 {   float myHalf = (bill + (bill * tax * .01) + (bill * tip * .01)) / 2.0;
     return myHalf;
 
