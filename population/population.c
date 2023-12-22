57477ebf026952eae94f93000e1ee50a825ef9fe
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-22 03:36:42 +0000

    Thu, Dec 21, 2023, 10:36 PM -05:00

--- population/population.c
+++ population/population.c
@@ -1,15 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
-    //Get starting population
+int main(void)  {
+    int populationStart;
 
-    int populationStart = get_int ("What is the starting  popuilation size of the llamas? ");
-    {
+    //Get starting population
+    do {
+        populationStart = get_int ("What is the starting  popuilation size of the llamas? ");
+
         if (populationStart < 9)
-            printf("Starting population must be 9 or greater!\n");
-    }
+            printf("Starting population must be 9 or greater. Please try again. \n");
+
+    } while (populationStart < 9);
+
+    return 0;
+
     //Get ending population
     int populationEnd = get_int ("what is the ending population size of the llamas? ");
 
