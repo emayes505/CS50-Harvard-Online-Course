48f78e9693447d9dd0ba45d1a64c6c2b1b5fc170
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-22 18:58:49 +0000

    Fri, Dec 22, 2023, 1:58 PM -05:00

--- population/population.c
+++ population/population.c
@@ -1,42 +1,38 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)  {
+int main(void)
+{
 
     int start, end, years = 0;
 
-    //Prompt for beginning population
+    // Prompt for beginning population
     do
     {
         start = get_int("What is the starting llama Population? ");
 
-        if (start < 9) printf("Starting population must be 9 or greater. Try again.\n");
+        if (start < 9)
+            printf("Starting population must be 9 or greater. Try again.\n");
+    }
+    while (start < 9);
 
-    } while (start < 9);
-
-    //Prompt for ending population
+    // Prompt for ending population
     do
     {
         end = get_int("What is the ending llama population? ");
 
-        if (end < start) printf("Ending population must be larger than starting population. Try again.\n");
-
-        else if (start == end)
-
-        return printf("Years: 0.\n");
-
-    //calculate num of years until starting population will reach ending population.
-
-    } while (end < start);
-
-    do {
-       start  = start + start / 3 - start / 4;
+        if (end < start)
+            printf("Ending population must be larger than starting population. Try again.\n");
+        // calculate num of years until starting population will reach ending population.
+    }
+    while (end < start);
+    do
+    {
+        start = start + start / 3 - start / 4;
         years++;
+    }
+    while (start < end);
 
-        } while (start < end);
-
-
-        //tell user how long it will take for start population to reach the end population.
-       printf("Years: %d.\n", years);
-
+    // tell user how long it will take for start population to reach the end population.
+    printf("Years: %d.\n", years);
 }
