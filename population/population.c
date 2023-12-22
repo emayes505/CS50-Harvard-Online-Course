c0119eeb879aaf522f184b7444ce83c97980a2fc
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-22 03:19:22 +0000

    Thu, Dec 21, 2023, 10:19 PM -05:00

--- population/population.c
+++ population/population.c
@@ -5,10 +5,13 @@ int main(void)
 {
     //Get starting population
     int populationStart = get_int ("What is the starting  popuilation size of the llamas? ");
+    {
+        if int < 9 
+    }
     //Get ending population
     int populationEnd = get_int ("what is the ending population size of the llamas? ");
-  
 
+    for (int i = )
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
