635451960a393393715a7b2c7a39f1fa6962884c
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-22 05:37:58 +0000

    Fri, Dec 22, 2023, 12:37 AM -05:00

--- population/population.c
+++ population/population.c
@@ -3,34 +3,21 @@
 
 int main(void)  {
 
-    int populationStart;
-    int populationEnd;
+    int start, end, years = 0
 
-    //Get starting population
+    //Prompt for beginning population
+    do
+    {
+        printf("Starting population: " );
+        scanf("%%d", &start)
 
-    do {
-        populationStart = get_int("What is the starting population? ");
-
-        if (populationStart < 9)
-        {
-            printf("Population size must be 9 or greater. Please try again.\n");
-        }
-        //if starting population is greater than 9 we can continue and get ending population.
-
-       } while (populationStart < 9);
-
-        populationEnd = get_int("What is the ending population? ");
+    }   while (start > 9);
 
 
 
-        // TODO: Calculate number of years until we reach threshold
-        for (int n = 1; i )
+
+
+
+
+
 }
-
-
-
-    // TODO: Calculate number of years until we reach threshold
-
-    // TODO: Print number of years
-
-
