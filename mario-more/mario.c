22911ff66ff2ae37d3aa4db7663cce65252931d5
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-22 21:31:16 +0000

    Fri, Dec 22, 2023, 4:31 PM -05:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -17,7 +17,7 @@ int main(void)
 
     for (row = 0; row < height; row++)
     {
-        for(column = 0; column < row; column++)
+        for(column = 0; column <= row; column++)
         printf("\n");
     }
 
