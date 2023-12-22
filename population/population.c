e39a1c8d4901607623978b60165946ac2d554408
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-22 03:39:28 +0000

    Thu, Dec 21, 2023, 10:39 PM -05:00

--- population/population.c
+++ population/population.c
@@ -8,18 +8,19 @@ int main(void)  {
     do {
         populationStart = get_int ("What is the starting  popuilation size of the llamas? ");
 
-        if (populationStart < 9)
-            printf("Starting population must be 9 or greater. Please try again. \n");
-
-    } while (populationStart < 9);
-
-    return 0;
 
     //Get ending population
     int populationEnd = get_int ("what is the ending population size of the llamas? ");
 
+        if (populationStart < 9)
+            printf("Starting population must be 9 or greater. Please try again. \n");
 
+    }while (populationStart < 9);
+
+    return 0;
+
+    
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
-}
+
