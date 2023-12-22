bb6ad67dafdc141c40fcd74ecf580aad6f7103b2
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-22 21:23:58 +0000

    Fri, Dec 22, 2023, 4:23 PM -05:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,10 +3,12 @@
 
 int main(void)
 {
-    int height
+    int height = 0;
     do
     {
     height = get_int("Height? ");
+    if (height < 1 || height > 8)
+        printf("Height must be between 1 and 8. Please try again.\n");
     }while (height < 1 || height > 8);
 
 
