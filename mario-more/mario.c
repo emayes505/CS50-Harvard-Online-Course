e8f3454f70bd7ae8ba977140a8f25fd665337a59
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-22 20:09:14 +0000

    Fri, Dec 22, 2023, 3:09 PM -05:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int hash = 0;
+
+    while (hash > 9 || hash < 1)
+    {
+         hash = get_int("How many blocks? ");
+
+         if (hash < 1 || hash > 9)
+             printf("The amount of blocks needs to be between 1 and 8.\n");
+     }
 }
