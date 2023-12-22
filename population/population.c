58a73cd83f583c01c4c9aea9887ed9752c03f389
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-22 04:57:59 +0000

    Thu, Dec 21, 2023, 11:57 PM -05:00

--- population/population.c
+++ population/population.c
@@ -7,13 +7,16 @@ int main(void)  {
     //Get starting population
 
     do {
-        populationStart = get_int("What is the starting population size of the llamas? ");
+        populationStart = get_int("What is the starting population? ");
 
         if (populationStart < 9) {
             printf("Population size must be 9 or greater. Please try again.\n");
         }
 
     } while (populationStart < 9);
+
+        int populationEnd = get_int("What is the ending population? ");
+
 
     // Now, populationStart contains a value greater than or equal to 9.
     // You can continue with the rest of your program.
