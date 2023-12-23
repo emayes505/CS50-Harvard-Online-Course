a0508f69c0799323841a809788b1f12af70dddc9
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-23 18:15:08 +0000

    Sat, Dec 23, 2023, 1:15 PM -05:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -5,6 +5,7 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#inclue <string.h>
 
 string replace (string input_string);
 
@@ -18,5 +19,30 @@ int main(int argc, string argv[])
 
 }
 string replace (string input_string)
+{
+    int length = strlen(input_string);
+    for (int i = 0; i < length; i++)
+    {
+        //input string[i]
+        switch(input_string[i])
+            case 'a':
+                input_string[i] = 6;
+                break;
+            case 'e':
+                input_string[i] = 3;
+                break;
+            case 'i':
+                input_string[i] = 1;
+                break;
+             case 'o':
+                input_string[i] = 0;
+                break;
+            case 'u':
+                input_string[i] = 6;
+                break;
 
-    for ()
+
+
+
+    }
+}
