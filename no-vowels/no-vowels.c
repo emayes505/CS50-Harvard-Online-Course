8a147a159af211967c433ee9b9497e00208a1049
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-23 19:20:26 +0000

    Sat, Dec 23, 2023, 2:20 PM -05:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -14,30 +14,40 @@ string replace(string input_string);
 int main(int argc, string argv[])
 {
     if (argc != 2)
-        {printf("Input not valid. Enter only 1 word after ./no-vowels. Try again.");
+        {
+        printf("Input not valid. Enter only 1 word after ./no-vowels. Try again.");
         return 1;
         }
     {
         printf("%s\n", replace (argv[1]));
     }
 
-
-string replace (input_string)
+}
+string replace (string input_string)
 {
     int length = strlen(input_string);
-    for (i=0, i < length, i++)
+    for (int i=0, i < length, i++)
         {
             switch (toupper(input_string))
-
+                {
                 case 'A':
                     input_string[i] = 54;
+                    break;
                 case 'E':
-                    input_string[i] =
+                    input_string[i] = 51;
+                    break;
                 case 'I':
+                    input_string[i] = 49;
+                    break;
                 case 'O':
+                    input_string[i] = 48;
+                    break;
+                default:
+                    break;
+                }
 
         }
+    return input_string;
 }
 
 
-}
