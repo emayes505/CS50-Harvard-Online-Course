3acf9080e5f2bef43942c76082d3e022ee788fa3
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-23 18:51:48 +0000

    Sat, Dec 23, 2023, 1:51 PM -05:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -8,40 +8,13 @@
 #include <stdio.h>
 #include <string.h>
 
-string replace(string input_string);
 
 int main(int argc, string argv[])
 {
     if (argc != 2)
-    {
-        printf("Use ./no-vowels 'word'. Try again.\n");
-        return 1;
-    }
+        printf("Input not valid. Enter only 1 word after ./no-vowels. Try again.")
+        
 
-    printf("%s\n", replace(argv[1]));
-}
-string replace(string input_string)
-{
 
-    int length = strlen(input_string);
-    for (int i = 0; i < length; i++)
-    {
-        // input string[i]
-        switch (toupper(input_string[i]))
-        {
-            case ('A'):
-                input_string[i] = 54;
-                break;
-            case 'E':
-                input_string[i] = 51;
-                break;
-            case 'I':
-                input_string[i] = 49;
-                break;
-            case 'O':
-                input_string[i] = 48;
-                break;
-        }
-    }
-    return input_string;
+
 }
