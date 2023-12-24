96ae6831e011b98021dddab5af83ca7937df3e29
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-24 04:13:18 +0000

    Sat, Dec 23, 2023, 11:13 PM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -8,13 +8,13 @@ int main(int argc, string argv[])
 int length = 0;
 bool keyIsAlpha = false;
 
+string key = get_string("%s", argv[1]);
+
 if (argv[1] == NULL)
         {
         printf("Invalid, no input. Please enter key.\n");
         }
         return 1;
-
-    string key = get_string("%s", argv[1]);
 
     length = strlen(key);
     do
