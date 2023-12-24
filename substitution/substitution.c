b3cb4d6a4c558bf72ebdd644c19ad34bbfad9934
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-24 17:52:36 +0000

    Sun, Dec 24, 2023, 12:52 PM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -15,21 +15,22 @@ int main(int argc, string argv[])
         printf("Invalid, no input. Please enter key.\n");
         return 1;
         }
-        for (int i = 0; i < length; i++)
-        {
-                if (!isalpha(key[i]))
-                {
-                        printf("Invalid input, all charcters must be alphabetical.\n");
-                        return 1;
                 for (int i = 0; i < length; i++)
                 {
                         for (int j = 1; j < length; j++ )
-                }
 
-                if (toupper(key[i]) == toupper(key[j]))
+                {
+                        if (toupper(key[i]) == toupper(key[j]))
+                       {
                         printf("Duplicates found. Try again.\n");
-                return 1;
+                        return 1;
+                        }
+                        else if (!isalpha(key[i]))
+                              {
+                              printf("Invalid input, all charcters must be alphabetical.\n");
+                              return 1;
+                              }
 
-                }
-        }
+                  }
+                  }
 }
