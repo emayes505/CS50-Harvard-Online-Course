3ce66160d826c81655be0aa381e56e4a6121164c
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-24 03:19:36 +0000

    Sat, Dec 23, 2023, 10:19 PM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,31 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
+
 {
+    int keySize = 0;
+    do
+    {
+    string key = get_string("Enter Key Here: \n");
 
-}
+    keySize = strlen(key);
+
+    if (keySize != 26)
+        {
+            printf("Key must contain 26 characters exactly. Try again.\n");
+            return 1;
+         }
+      } while (keySize != 26);
+
+
+
+    }
+
+
+
+
+
+
