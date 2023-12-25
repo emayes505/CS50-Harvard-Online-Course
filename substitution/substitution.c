6ec51b939bd7a2ed635ee2189eb3d809273a7ebe
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-25 04:17:43 +0000

    Sun, Dec 24, 2023, 11:17 PM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -13,7 +13,7 @@ int length = strlen(argv[1]);
                 printf("Usage: ./substitution 'key' .\n");
                 return 1;
         }
-        if (argv [1] != 26)
+        if (length != 26)
         {
                 printf("Key must contain 26 characters.\n");
                 return 1;
