efc2e56c3466884fba1813ba818fcb16f93175d6
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-25 01:16:55 +0000

    Sun, Dec 24, 2023, 8:16 PM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,52 +6,20 @@
 int main(int argc, string argv[])
 
 {
-  string key = argv[1];
-  string pText;
-  int length = strlen(key);
-  string changeText(string pText)
-
-
-
-        if (argv[1] == NULL || length != 26 || argc != 2)
+        string key = argv[1];
+        int len = strlen(key);
+        // input parameters are only 2 inputs/and a single key value of 26 characters.
+        if (argc =! 2 || argv[1] == NULL || len != 26)
         {
-        printf("Invalid, no input. Please enter key.\n");
-        return 1;
-        }
-                for (int i = 0; i < length; i++)
-                {
-                        for (int j = i + 1; j < length; j++ )
-
-                        {
-                        if (toupper(key[i]) == toupper(key[j]))
-                       {
-                        printf("Duplicates found. Try again.\n");
-                        return 1;
-                        }
-                        else if (!isalpha(key[i]))
-                              {
-                              printf("Invalid input, all charcters must be alphabetical.\n");
-                              return 1;
-                              }
-                        }
-                  pText = get_string("plaintext: ");
-                  }
-
-                for (int i = 0; i < length; i++)
-           {
-
-                if(islower(key[i]))
-                {
-                        key[i] = key[i] - 32;
-                }
-
-
-        string changeText(string pText)
+                printf("Input is invalid. Enter key after program command ./substitution KEYHERE(26 Characters).\n");
+                return 1;
+        }// iterate through key to determine is duplicates are present. 
+        for (int i = 0; i < len; i++)
         {
-        for (int i = 0; i < length; i++)
+                for(int j = i + 1; i < len ; j++)
+        }
 
 
-        }
-        }
+
 
 }
