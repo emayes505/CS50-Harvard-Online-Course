2b75f1fc7d16f73c6ad9f7dc9ea6d0395f3b4c67
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-25 22:36:09 +0000

    Mon, Dec 25, 2023, 5:36 PM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,49 +1,47 @@
-#include <ctype.h>
 #include <cs50.h>
-#include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-if (argc != 2)
+    if (argc != 2)
     {
         printf("Invalid input. Usage: ./caesar 'key'.\n");
         return 1;
     }
 
-for (int i = 0, keyLength = strlen(argv[1]); i < keyLength; i++)
+    for (int i = 0, keyLength = strlen(argv[1]); i < keyLength; i++)
     {
-        if(!isdigit(argv[1] [i]))
+        if (!isdigit(argv[1][i]))
         {
             printf("Key must only contain digits.\n");
             return 1;
         }
     }
-string plainText = get_string("plaintext: ");
-int key = atoi(argv[1])%26;
+    string plainText = get_string("plaintext: ");
+    int key = atoi(argv[1]) % 26;
 
+    printf("ciphertext: ");
 
-        printf("ciphertext: ");
-
-for (int i = 0, length = strlen(plainText); i < length; i++)
+    for (int i = 0, length = strlen(plainText); i < length; i++)
     {
         if (isupper(plainText[i]))
         {
-         int index = plainText[i] - 65 + key;
-         printf("%c", toupper((index%26 + 65)));
+            int index = plainText[i] - 65 + key;
+            printf("%c", toupper((index % 26 + 65)));
         }
         else if (islower(plainText[i]))
         {
             int index = plainText[i] - 97 + key;
-            printf("%c", tolower(index%26 + 97));
+            printf("%c", tolower(index % 26 + 97));
         }
         else
-            {
-                printf("%c", plainText[i]);
-            }
+        {
+            printf("%c", plainText[i]);
+        }
     }
     printf("\n");
     return 0;
-
 }
