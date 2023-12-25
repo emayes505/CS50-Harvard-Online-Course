ffd33892d620440508ab0f658155108fe91f6e78
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-25 05:25:13 +0000

    Mon, Dec 25, 2023, 12:25 AM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,78 +3,72 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (int argc, string argv[])
+int main(int argc, string argv[])
 
 {
-int length = strlen(argv[1]);
+    int length = strlen(argv[1]);
 
-        if (argc != 2 || argv[1] == NULL)
+    if (argc != 2 || argv[1] == NULL)
+    {
+        printf("Usage: ./substitution 'key' .\n");
+        return 1;
+    }
+    if (length != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+    string key = argv[1];
+    for (int i = 0; i < length; i++)
+    {
+        if (!isalpha(key[i]))
         {
-                printf("Usage: ./substitution 'key' .\n");
+            printf("Invalid input. All characters must be alphabetic.\n");
+            return 1;
+        }
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        for (int j = i + 1; j < 26; j++)
+        {
+            if (toupper(key[i]) == toupper(key[j]))
+            {
+                printf("Duplicates characters.\n");
                 return 1;
+            }
         }
-        if (length != 26)
-        {
-                printf("Key must contain 26 characters.\n");
-                return 1;
-        }
-        string key = argv[1];
-        for (int i = 0; i < length; i++)
-                {
-                if (!isalpha(key[i]))
-                        {
-                                printf("Invalid input. All characters must be alphabetic.\n");
-                                return 1;
-                        }
-                }
-        for (int i = 0; i < 26; i++)
-        {
-                for (int j = i + 1; j < 26; j++)
-                {
-                        if (toupper(key[i]) == toupper(key[j]))
-                        {
-                                printf("Duplicates characters.\n");
-                                return 1;
-                        }
-                }
-        }
-        string plainText = get_string("plaintext: ");
+    }
+    string plainText = get_string("plaintext: ");
 
-        int plength = strlen(plainText);
+    int plength = strlen(plainText);
 
-        for (int i = 0; i < 26; i++)
+    for (int i = 0; i < 26; i++)
+    {
         {
-                {
-                if (!isupper(key[i]))
+            if (!isupper(key[i]))
 
                 key[i] = key[i] - 32;
-                }
         }
-                printf("%s", key);
-                printf("ciphertext: ");
+    }
+    printf("%s", key);
+    printf("ciphertext: ");
 
-
-        for (int i = 0; i < plength; i++)
+    for (int i = 0; i < plength; i++)
+    {
+        if (islower(plainText[i]))
         {
-                if (islower(plainText[i]))
-                {
-                        int character = plainText[i] - 97;
-                        printf("%c", tolower(key[character]));
-
-                }
-                else if (isupper(plainText[i]))
-                {
-                        int character = plainText[i] - 65;
-                        printf("%c", key[character]);
-                }
-                else
-                {
-                        printf("%c", plainText[i]);
-                }
-
+            int character = plainText[i] - 97;
+            printf("%c", tolower(key[character]));
         }
-                printf("\n");
-
-
-
+        else if (isupper(plainText[i]))
+        {
+            int character = plainText[i] - 65;
+            printf("%c", key[character]);
+        }
+        else
+        {
+            printf("%c", plainText[i]);
+        }
+    }
+    printf("\n");
 }
