b9a70a805fb78ded9a6c12fb7dfaa6376a77bea3
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-25 01:21:41 +0000

    Sun, Dec 24, 2023, 8:21 PM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,15 +9,26 @@ int main(int argc, string argv[])
         string key = argv[1];
         int len = strlen(key);
         // input parameters are only 2 inputs/and a single key value of 26 characters.
-        if (argc =! 2 || argv[1] == NULL || len != 26)
+        if (argc != 2 || argv[1] == NULL || len != 26)
         {
                 printf("Input is invalid. Enter key after program command ./substitution KEYHERE(26 Characters).\n");
                 return 1;
-        }// iterate through key to determine is duplicates are present. 
+        }// iterate through key to determine is duplicates are present.
         for (int i = 0; i < len; i++)
         {
                 for(int j = i + 1; i < len ; j++)
+        if (toupper(key[i]) == toupper(key [j]))
+        {
+                printf("Duplicates found in key. Try again.\n");
+                return 1;
         }
+        else if (!isalpha(key[i]))
+        {
+                printf("Key contains invalid characters. Try again.");
+                return 1;
+        }
+        }
+
 
 
 
