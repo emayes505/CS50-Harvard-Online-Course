721510639770f458ff7892d665ced93072304278
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-25 16:59:22 +0000

    Mon, Dec 25, 2023, 11:59 AM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,45 +6,47 @@
 int main(int argc, char *argv[])
 
 {
-string key = argv[1];
-int length = strlen(key);
 if (argc != 2)
     {
         printf("Usage: ./solution key\n");
         return 1;
     }
+    // cannot use argv[1] until we check to make sure it exsists, else we get segmentation fault.
+    string key = argv[1];
+    int length = strlen(key);
 if (length!= 26)
     {
         printf("Key must contain 26 characters.\n");
         return 1;
-    }
-    for (int i = 0; i < 26; i++)
+    } // change key to all upper, also check to make sure all chars are alpha.
+for (int i = 0; i < 26; i++)
+    {
+        if (islower(key[i]))
         {
-            if (islower(key[i]))
-            {
-                key[i] = key[i] - 32;
-            }
-            else if (!isalpha(key[i]))
-            {
-                printf("All characters must be alphabetic.\n");
-                return 1;
-            }
+            key[i] = key[i] - 32;
         }
-     for (int i = 0; i < 26; i++)
-     {
+        else if (!isalpha(key[i]))
+        {
+            printf("All characters must be alphabetic.\n");
+            return 1;
+        }
+        } //check for duplicates in key.
+for (int i = 0; i < 26; i++)
+        {
             for (int j = i + 1; j < 25; j++)
             {
-            if ((key[i]) == (key[j]))
-            {
-                printf("Duplicate in key.\n");
-                return 1;
-            }
-            }
-     }
+                 if ((key[i]) == (key[j]))
+                 {
+                    printf("Duplicate in key.\n");
+                    return 1;
+                 }
+             }
+     } // get plaintext.
      string plainText = get_string("plaintext: ");
      {
         printf("ciphertext: ");
      }
+     // change plaintext to cipher.
     int plength = strlen(plainText);
     for (int i = 0; i < plength; i++)
     {
