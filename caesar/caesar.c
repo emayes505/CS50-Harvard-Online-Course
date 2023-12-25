93898038a5589f71195572e09bd293481bf2c253
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-25 21:22:31 +0000

    Mon, Dec 25, 2023, 4:22 PM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+if (argv[1] != 2)
+    printf("Invalid input. Usage: ./caesar 'key'.\n");
+    return 1;
 }
