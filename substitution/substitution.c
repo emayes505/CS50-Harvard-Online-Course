d703a5e7c5cb077c76bca990c174c358c934d5ae
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-25 03:42:21 +0000

    Sun, Dec 24, 2023, 10:42 PM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,74 +3,29 @@
 #include <stdio.h>
 #include <string.h>
 
-string changeText(string plainText, string key);
-
-int main(int argc, string argv[])
-
-
+int main (int argc, string argv[])
 
 {
+int length = strlen(argv[1]);
+
+        if (argc != 2, length != 26)
+        {
+                printf("Usage: ./substitution 'key' .\n");
+                return 1;
+        }
         string key = argv[1];
-        int len = strlen(key);
-        // input parameters are only 2 inputs/and a single key value of 26 characters.
-
-        if (argc != 2 || len != 26)
+        for (int i = 0; i < length; i++)
         {
-                printf("Input is invalid. Enter key after program command ./substitution KEYHERE(26 Characters).\n");
-                return 1;
-        }// iterate through key to determine is duplicates are present.
-        for (int i = 0; i < len; i++)
-        {
-                for(int j = i + 1; j < len ; j++)
+                if (!isalphakey[i])
                 {
-                        if (toupper(key[i]) == toupper(key [j]))
-                        {
-                                printf("Duplicates found in key. Try again.\n");
-                                return 1;
-                         }
-        for (int k = 0; k < len; k++)
-        {
-        if (!isalpha(key[k]))
-        {
-                printf("Key contains invalid characters. Try again.");
-                return 1;
-        }
-        }
-                }
-        return 0;
-        }
-                string plainText = get_string("Plain Text: \n");
-        {
-
-        }
-        string changeText(string plainText, string key);
-        {
-                int length = strlen(plainText);
-                char cipher;
-
-                for (int i = 0; i < length; i++)
-                {
-                        if (isalpha(plainText[i]))
-                        {
-                           if (isupper(plainText[i]))
-                           for ( i = 0; i < length; i++)
-                           {
-                                cipher =  plainText[i] - 65;
-                           }
-                           else
-                           {
-                                cipher =  plainText[i] - 97;
-                           }
-                           {
-
-                                return cipher;
-                           }
-
-                        }
-                else
-                        printf("All characters entered must be alphabetical. Try again\n");
-
+                        printf("Invalid input. All characters must be alphabetic.\n");
+                        return 1;
                 }
         }
+
+
+
+
+
 
 }
