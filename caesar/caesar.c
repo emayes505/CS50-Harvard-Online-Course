61b8b5338af7f0067cecb8886216ae5a8c34e7d2
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-25 21:28:18 +0000

    Mon, Dec 25, 2023, 4:28 PM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,9 +1,16 @@
+#include <ctype.h>
 #include <cs50.h>
 #include <stdio.h>
 
 int main(int argc, string argv[])
 {
 if (argv[1] != 2)
-    printf("Invalid input. Usage: ./caesar 'key'.\n");
-    return 1;
+    {
+        printf("Invalid input. Usage: ./caesar 'key'.\n");
+        return 1;
+    }
+    int key = get_int("Key: ");
+
+if (!isdigit(key))
+
 }
