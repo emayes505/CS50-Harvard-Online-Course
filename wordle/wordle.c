68bfd3a15a5e431468c204deccef5a8249ea33f6
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-26 02:03:28 +0000

    Mon, Dec 25, 2023, 9:03 PM -05:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -33,7 +33,6 @@ int main(int argc, string argv[])
         printf("Invalid input. Try again.\n");
         return 1;
     }
-
     int wordsize = 0;
     int length = strlen(argv[1]);
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
