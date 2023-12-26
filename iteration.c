535e517938f922ecf385efcc03306812332b96fb
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-26 17:32:04 +0000

    Tue, Dec 26, 2023, 12:32 PM -05:00

--- iteration.c
+++ iteration.c
@@ -1,20 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
-void draw(int n);
+
 int main(void)
 {
-    int height = get_int("Height: ");
-    draw(height);
+    draw(1);
 }
 
-void draw (int n)
+void draw(int n)
 {
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
+        printf("#");
+        draw (n+1);
     }
+    printf("\n");
 }
