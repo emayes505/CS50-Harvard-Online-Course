ae6d880c483676ef4270c99fe72201313c0bce50
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-26 02:57:54 +0000

    Mon, Dec 25, 2023, 9:57 PM -05:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -82,11 +82,7 @@ int main(int argc, string argv[])
 
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
-        for (int i = 0; i < wordsize; i ++)
-        {
-            if (guess[i] == choice[i])
-        }
-
+        
 
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
