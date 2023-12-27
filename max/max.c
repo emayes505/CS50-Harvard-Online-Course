28c52138fd35de8275f63e2d4bc5b13117427ac3
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-27 01:28:26 +0000

    Tue, Dec 26, 2023, 8:28 PM -05:00

--- max/max.c
+++ max/max.c
@@ -11,7 +11,7 @@ int main(void)
     do
     {
         n = get_int("Number of elements: ");
-    } 
+    }
     while (n < 1);
 
     int arr[n];
@@ -27,5 +27,13 @@ int main(void)
 // TODO: return the max value
 int max(int array[], int n)
 {
-    return 0;
+    int max = array[0];
+
+    for (int i = 0; i < n ; i++)
+
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    return max;
 }
