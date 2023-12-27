6567a00a0d712c4728f1c7f17c719804d1a48fbc
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-27 16:19:13 +0000

    Wed, Dec 27, 2023, 11:19 AM -05:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -98,9 +98,10 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
+
     for (int i = 0; i < candidate_count, i++)
     {
-        if (name)
+        rank i + 1 = ranks[i];
     }
     // TODO
     return false;
