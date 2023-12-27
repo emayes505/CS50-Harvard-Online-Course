1bb34f00348ddf223a82021a242ae82c24618672
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-27 22:12:06 +0000

    Wed, Dec 27, 2023, 5:12 PM -05:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -83,7 +83,11 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    
+    for (int i = 0; i < candidate_count, i++)
+    {
+        candidate[i]
+    }
+
     // TODO
     return;
 }
