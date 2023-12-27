21c82aaf082c4144f4f8acd4ea592d89fd255de5
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-27 14:31:41 +0000

    Wed, Dec 27, 2023, 9:31 AM -05:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -95,7 +95,8 @@ void add_items(void)
 // Search through the menu array to find an item's cost
 float get_cost(string item)
 {
-    for (int i = 0; i < NUM_ITEMS; i++){
+    for (int i = 0; i < NUM_ITEMS; i++)
+    {
     if (strcasecmp(item, menu[i].item) == 0)
     {
         return menu[i].price;
