dc0bff94dd7b63b8d6d70a33e90b3567702d1ff3
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-27 01:29:45 +0000

    Tue, Dec 26, 2023, 8:29 PM -05:00

--- max/max.c
+++ max/max.c
@@ -29,11 +29,11 @@ int max(int array[], int n)
 {
     int max = array[0];
 
-    for (int i = 0; i < n ; i++)
+    for (int i = 0; i < n; i++)
 
-    if (array[i] > max)
-    {
-        max = array[i];
-    }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
     return max;
 }
