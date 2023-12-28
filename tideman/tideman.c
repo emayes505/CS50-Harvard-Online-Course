e104dddf5bdfeb09225d3abf6b7fbf95b9bcd538
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-28 17:45:57 +0000

    Thu, Dec 28, 2023, 12:45 PM -05:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -113,7 +113,10 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < voter_count; j++)
+    }
 
     return;
 }
