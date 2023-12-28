7bfb910189ada2764f63ff743d2b3985d237cef0
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-28 04:39:54 +0000

    Wed, Dec 27, 2023, 11:39 PM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -159,7 +159,16 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    int maxVotes = candidates[0].votes;
+    for (int i = 1; i < candidate_count - 1; i++)
+    {
+        if (candidates[i].votes > maxVotes)
+        {
+            maxVotes = candidates[i].votes;
+        }
+        if (maxVotes > candidate_count // 2)
+    }
+   printf("%s" , candidates[i].names)
     return false;
 }
 
