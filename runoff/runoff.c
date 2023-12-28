679fa82be4e767eb3dee01b851df0ef7002e4901
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-28 03:21:41 +0000

    Wed, Dec 27, 2023, 10:21 PM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
     for (int i = 0; i < candidate_count; i++)
     if (strcasecmp(name, candidates[i].name) == 0)
     {
-        preferences[i][j] = 
+        preferences[voter][rank] =
         return true;
     }
     else
