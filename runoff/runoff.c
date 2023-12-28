f98debfc8a1b5514705b1a46f1363aa4668d6c30
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-28 05:36:53 +0000

    Thu, Dec 28, 2023, 12:36 AM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -194,9 +194,11 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    for (int i = 0; i < voter_count; i++)
+    for (int i = 0; i < voter_count - 1; i++)
     {
         if (candidates[i].eliminated == false)
+       
+
     }
 
 
