760207f1d8b2ecfdb6c10e257859f3ff1e4b1e9d
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-28 16:50:04 +0000

    Thu, Dec 28, 2023, 11:50 AM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -196,14 +196,14 @@ bool is_tie(int min)
 {
     for (int i = 0; i < voter_count; i++)
     {
-        if (candidates[i].eliminated == false && candidates[i].votes == min)
+        if (candidates[i].eliminated == true)
         continue;
     }
     else
     {
-        return false;
+
     }
-    return true; 
+    return true;
 }
 
 
