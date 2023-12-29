94d06d83dccb0d70179a9d33a05070a4a09e9ee9
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-29 20:45:25 +0000

    Fri, Dec 29, 2023, 3:45 PM -05:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -162,6 +162,7 @@ void sort_pairs(void)
     for (int i = 0; i < pair_count; i++)
     {
       int difference[candidate_count];
+      int 
       difference[i] = preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner];
     }
     for (int j = 0; j < pair_count; j++)
