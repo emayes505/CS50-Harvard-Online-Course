1122db4d2c6b334218070626f7e070f07b03d884
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-30 03:12:50 +0000

    Fri, Dec 29, 2023, 10:12 PM -05:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -184,16 +184,18 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
+    if(pairs[pair_count - 1].loser == pairs[0].winner)
     for (int i = 0; i < pair_count; i++)
     {
-    if (pairs[i].loser == pairs[i + 1].winner)
-    {
-        continue;
-    }
-    else
+    if (pairs[i].loser != pairs[i + 1].winner)
     {
         break;
     }
+    else
+    {
+        continue;
+    }
+
         pairs[pairs[pair_count - 1]] = false;
 
     }
