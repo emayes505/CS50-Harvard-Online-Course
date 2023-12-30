478b137a2a786668c18c6bcdf8217a75eebcd9d0
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-30 02:50:29 +0000

    Fri, Dec 29, 2023, 9:50 PM -05:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -185,7 +185,7 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     for (int i = 0; i < pair_count; i++)
-    if (pairs[i].winner = )
+    if (pairs[pair_count - 1] == pairs[])
     {
         locked[pairs[i].winner][pairs[i].loser] = true;
     }
