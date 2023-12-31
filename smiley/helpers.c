92762f6159d7564d5c49c1e5fb9d4cd7a484b857
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-31 18:26:27 +0000

    Sun, Dec 31, 2023, 1:26 PM -05:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -6,7 +6,12 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width - 1; j++)
         {
-            if (image[i][j] == 0xFF,0xFF,0xFF)
+            if (image[i][j].rgbtRed == 0xFF && image[i][j].rgbtBlue == 0xFF && image[i][j].rgbtGreen == 0xFF)
+            {
+                image[i][j].rgbtRed   = 0xB6;
+                image[i][j].rgbtBlue  = 0xF7;
+                image[i][j].rgbtGreen = 0x11;
+            }
         }
     }
     // Change all black pixels to a color of your choosing
