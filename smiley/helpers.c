dad8db603b202b2623963963fd7cdef64ce97e66
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-31 18:19:48 +0000

    Sun, Dec 31, 2023, 1:19 PM -05:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,7 +2,7 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0, i < height - 1; i++)
+    for (int i = 0; i < height - 1; i++)
     {
         for(int j = 0; j < width - 1; j++)
         {
