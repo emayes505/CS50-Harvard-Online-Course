790e959b0b65db86576dd79e89a95c524c7cf6f6
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-31 19:12:53 +0000

    Sun, Dec 31, 2023, 2:12 PM -05:00

--- volume/volume.c
+++ volume/volume.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
     uint8_t copyHeader[HEADER_SIZE];
-    copyBuffer = fread(copyHeader, HEADER_SIZE, 1, input);
+    fread(copyHeader, HEADER_SIZE, 1, input);
 
 
 
