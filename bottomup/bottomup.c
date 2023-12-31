f50c8b0e6d7f01a50d91aa9f83ac34e9534ce5bc
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-31 02:06:05 +0000

    Sat, Dec 30, 2023, 9:06 PM -05:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
         return 4;
     }
 
-    bi.height = - bi.height; 
+    bi.biHeight = - bi.biHeight;
     // Write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
