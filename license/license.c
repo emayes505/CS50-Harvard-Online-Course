d4f4521fea6a853bd91aed9f40722effe9a0511b
emayes505 <153453839+emayes505@users.noreply.github.com>
2023-12-31 03:24:38 +0000

    Sat, Dec 30, 2023, 10:24 PM -05:00

--- license/license.c
+++ license/license.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
         buffer[6] = '\0';
 
         // Save plate number in array
-        strcpy(plates[idx], buffer);
+        malloc(strcpy(plates[idx], buffer));
         idx++;
     }
 
