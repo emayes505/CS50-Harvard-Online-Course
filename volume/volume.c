1386ba2b8b408401efb248b2ff77a136fc2a4aa8
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-01 06:33:55 +0000

    Mon, Jan 1, 2024, 1:33 AM -05:00

--- volume/volume.c
+++ volume/volume.c
@@ -41,12 +41,13 @@ int main(int argc, char *argv[])
     fwrite(header, HEADER_SIZE, 1, output);
 
     int16_t buffer;
-    while (fread())
+    while (fread(&buffer, sizeof(int16_t), 1, input) != 0)
     {
-        fread(&buffer, sizeof(int16_t), 1, input);
+        buffer *= factor;
+        fwrite(&buffer,sizeof(int16_t), 1, output);
     }
-    buffer *= factor;
-    fwrite(&buffer,sizeof(int16_t), 1, output);
+
+
 
 
 
