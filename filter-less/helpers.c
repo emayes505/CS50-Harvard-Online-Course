038fb205ec23f7df37e6c6b8bfb3ebbfcbb367eb
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-01 19:33:17 +0000

    Mon, Jan 1, 2024, 2:33 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -75,7 +75,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0;  j < width; j++)
         {
-            image[i][j] = image[i][height - j];
+            image[i][j] = image[i][width - j];
         }
     }
     return;
