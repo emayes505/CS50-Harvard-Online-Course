c6b2ad35e69d276731555329ed586f7a873eb430
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-01 20:03:31 +0000

    Mon, Jan 1, 2024, 3:03 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -78,16 +78,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0;  j < width; j++)
         {
-            int swap = width - j - 1;
-            tempRed[j] = image[i][swap].rgbtRed;
-            tempBlue[j] = image[i][swap].rgbtBlue;
-            tempGreen[j] = image[i][swap].rgbtGreen;
+            int swap = width - j;
+            tempRed[swap] = image[i][j].rgbtRed;
+            tempBlue[swap] = image[i][j].rgbtBlue;
+            tempGreen[swap] = image[i][j].rgbtGreen;
 
         image[i][j].rgbtRed = tempRed[j];
         image[i][j].rgbtBlue = tempBlue[j];
         image[i][j].rgbtGreen = tempGreen[j];
         }
-        
+
     }
 
     return;
