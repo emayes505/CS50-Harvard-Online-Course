933681e17322961c251327830e19f313c343b544
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-01 18:14:11 +0000

    Mon, Jan 1, 2024, 1:14 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,12 +8,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-         float average = round(image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue/3);
+         float average = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue;
+         int averagePixel = round((average) / 3);
 
-
-         image[i][j].rgbtRed   = average;
-         image[i][j].rgbtGreen = average;
-         image[i][j].rgbtBlue  = average;
+         image[i][j].rgbtRed   = averagePixel;
+         image[i][j].rgbtGreen = averagePixel;
+         image[i][j].rgbtBlue  = averagePixel;
         }
     }
     return;
@@ -32,7 +32,6 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             }
             else
             {
-            
                 newRed = 255;
             }
         float newGreen = image[i][j].rgbtRed * .349 + image[i][j].rgbtGreen * .686 + image[i][j].rgbtBlue * .168;
@@ -53,7 +52,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             {
                 newBlue = 255;
             }
-    }
+        }
     }
     return;
 }
