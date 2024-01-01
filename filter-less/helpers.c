0d78857d5cd94c98535e29ff7da9c30aa8e16a32
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-01 20:35:01 +0000

    Mon, Jan 1, 2024, 3:35 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -100,7 +100,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j]
+            image[i][j].rgbtRed
+            image[i][j].rgbtGreen
+            image[i][j].rgbtBlue
         }
     }
     return;
