0b030b07834458cb508a66b61e35a08ac1275551
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-01 07:29:23 +0000

    Mon, Jan 1, 2024, 2:29 AM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width - 1; j++)
         {
-         int average = (RGBTRIPLE.rgbtBlue + RGBTRIPLE.rgbtGreen + RGBTRIPLE.rgbtRed) / 3;
+         int average = ((RGBTRIPLE.rgbtBlue + RGBTRIPLE.rgbtGreen + RGBTRIPLE.rgbtRed) / 3);
          RGBTRIPLE.rgbtBlue  = average;
          RGBTRIPLE.rgbtGreen = average;
          RGBTRIPLE.rgbtRed = average;
