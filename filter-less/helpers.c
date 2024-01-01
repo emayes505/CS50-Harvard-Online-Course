4be20f523985481c4717fc80c74ffc988b2b9462
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-01 22:18:27 +0000

    Mon, Jan 1, 2024, 5:18 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -112,9 +112,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 for (int k = 0; k = 2; k++)
                 if (copyGrid[i + k - 1][j].rgbtRed != NULL)
                 {
-                int row1 = copyGrid[i + k - 1][j].rgbtRed
+                int midRow = copyGrid[i + k - 1][j].rgbtRed
                 }
-                = copyGrid[i][j + k - 1].rgbtRed
+                int= copyGrid[i][j + k - 1].rgbtRed
                 = copyGrid[i + k - 1][j + k - 1].rgbtRed
             }
 
