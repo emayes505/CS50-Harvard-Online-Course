d6d6c7bb1e85ce1a11e8e860fd660235b18444a4
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-02 03:33:53 +0000

    Mon, Jan 1, 2024, 10:33 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+ //open memory card > look for beginning of the JPEG > open a new JPEG file to write to, write data in 512byte chunks 
 int main(int argc, char *argv[])
 {
- 
+
 }
