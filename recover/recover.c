b27ea8f82a32025f613234e4ba3a8d0e6497a7b8
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-02 04:14:02 +0000

    Mon, Jan 1, 2024, 11:14 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,7 +12,6 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 512, 1, fmc) != 0);
         {
-            buffer[3] & 0xF0 == 0xE0
             if (buffer[0] == 0xFF && buffer[1] == 0xd8 && buffer[2] == 0xFF && (buffer[3] & 0xF0 == 0xE0) == 0xE0)
             {
 
