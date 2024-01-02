f0f52335855eb7751d4e60830003f96cd1d643c0
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-02 18:23:59 +0000

    Tue, Jan 2, 2024, 1:23 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
 
     const char *card = "card.raw";
-    FILE *fmc = fopen(card, "r");
+    FILE fmc = fopen(card, "r");
     int buffer[512];
 
     while (fread(buffer, 512, 1, fmc) != 0)
