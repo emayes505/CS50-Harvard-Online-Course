ff15261cb6fcfc0aee4e6562e85466c52a8bd323
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-02 19:21:30 +0000

    Tue, Jan 2, 2024, 2:21 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,8 +23,11 @@ int main(int argc, char *argv[])
     uint8_t buffer[512];
     while (fread(buffer, 1, 512, mcFile) != 0)
     {
-        sprintf(buffer, "%03i", fileNum)
-
+        if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && (buffer[3] & 0xF0) == 0xE0)
+        {
+        sprintf(buffer, "%03i.jpeg", fileNum);
+        fwrite(buffer, 512, 1, "%03i.jpeg" , fileNum)
+        }
     }
 fileNum++;
 
