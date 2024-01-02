5810e7e9d961167806acf8f5480be23dafbecf14
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-02 04:20:01 +0000

    Mon, Jan 1, 2024, 11:20 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
         {
             if (buffer[0] == 0xFF && buffer[1] == 0xd8 && buffer[2] == 0xFF && (buffer[3] & 0xF0) == 0xE0)
             {
-                sprintf()
+                sprintf(filename, "%03i.jpeg", )
                 fwrite()
 
             }
