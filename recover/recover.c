cb4d5e2a5d00cb57e8c65a5f404b7c2eb31d58e6
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-02 03:36:57 +0000

    Mon, Jan 1, 2024, 10:36 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
- //open memory card > look for beginning of the JPEG > open a new JPEG file to write to, write data in 512byte chunks 
+ //open memory card > look for beginning of the JPEG > open a new JPEG file to write to, write data in 512byte chunks until new jpeg is found. then close and start new repeat. until end of file is reached.
+// image is a jpeg. first byte is 0xff, 0xd8, 0xff, 0xe(?)/when noticing this patten we know this is beginning of jpeg. last byte is oxe0/0xe1/oxe2...oxef
 int main(int argc, char *argv[])
 {
+    FILE *f = fopen(card.raw, "r")
 
 }
