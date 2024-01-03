0b7bb56d652b82d48ea784bfcf8c2c726dac0475
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-03 07:17:09 +0000

    Wed, Jan 3, 2024, 2:17 AM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,8 +20,7 @@ int main(int argc, char *argv[])
     {
         printf("error opening file.\n");
     }
-    uint8_t buffer;
-    while (uint8_t *buffer = calloc(fread(buffer, 1, 512, mcFile) != 0))
+    while (uint8_t buffer fread(buffer, 1, 512, mcFile) != 0)
     {
         char fileName[9];
         if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && (buffer[3] & 0xF0) == 0xE0)
