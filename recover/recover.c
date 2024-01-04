8a3b32970bfceef435487d2faed822ab393837a5
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-04 07:07:51 +0000

    Thu, Jan 4, 2024, 2:07 AM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,12 +28,12 @@ int main(int argc, char *argv[])
     {
 
         char fileName[9];
-        if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && (buffer[3] & 0xF0) == 0xE0 && isOpen == false)
+        if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && (buffer[3] & 0xF0) == 0xE0)
         {
         close(img);
         isOpen = false;
-        fileNum++;
         sprintf(fileName, "%03i.jpg", fileNum);
+        fileNum++;
         img = fopen(fileName, "w");
         fwrite(buffer, 512, 1, img);
         isOpen = true;
