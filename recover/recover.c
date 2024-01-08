5310f8c12b399a49eb76ed26092df4a0e62ca748
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-08 04:59:18 +0000

    Sun, Jan 7, 2024, 11:59 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     {
         if (isBufferJpeg((char *)buffer, isOpen) == true && isOpen == true){
         fclose(img);
-        newFile(fileNum, char fileName);
+        newFile(fileNum, fileName);
         img = fopen(fileName, "w");
         isOpen = true;
         }
