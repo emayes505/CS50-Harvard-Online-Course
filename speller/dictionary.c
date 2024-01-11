aec182608fdc3185e8dc5444c5fb40cf2c29c5c7
emayes505 <153453839+emayes505@users.noreply.github.com>
2024-01-11 21:00:57 +0000

    Thu, Jan 11, 2024, 4:00 PM -05:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -37,6 +37,7 @@ unsigned int hash(const char *word)
 //hash word to obtain hash value, insert node into hash table at location
 bool load(const char *dictionary)
 {
+    
     // TODO
     return false;
 }
