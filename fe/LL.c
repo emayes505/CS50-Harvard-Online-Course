bf3f21e3dc4514e4a0517b0aa377d832c6dc1ed6
Erik Mayes <emayes528@gmail.com>
2024-08-03 23:02:35 +0000

    Sat, Aug 3, 2024, 11:02 PM +00:00

---
+++ fe/LL.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+}Node;
+
+
+void pringList(struct Node *head);
+Node *linkedList(int arr[], int size);
+
+int main() {
+    int arr[] = {1, 7, 5, 2};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    struct Node *head = makeLL(arr, size);
+    printList(head);
+}
+Node *linkedList(int arr[], int size) {
+    struct Node *head = NULL;
+    struct Node *tail;
+    for (int i = 0; i < size; i++) {
+        struct Node newNode = malloc(sizeof(struct Node));
+        newNode->data = arr[i];
+        newNode->next = NULL;
+
+    if (head == NULL) {
+        head = newNode;
+        tail = newNode;
+    }
+    else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+    }
+void pringList(struct Node *head) {
+    while (head->next != NULL) {
+
+        printf("%d", head->data);
+    if (head->next->next != NULL) {
+        printf("->");
+    }
+}
+}
+}
+
