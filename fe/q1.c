b981b1d3406f210626e4816013ad42b62333809e
Erik Mayes <emayes528@gmail.com>
2024-08-03 22:42:42 +0000

    Sat, Aug 3, 2024, 10:42 PM +00:00

---
+++ fe/q1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int ** arr1 = malloc(3 * sizeof(int *));
+    for(int i = 0; i < 3 i++){
+        arr[i] = malloc(2 * sizeof(int));
+        int *arr 2 = malloc(3 * 2 * sizeof(int));
+    }
+}
