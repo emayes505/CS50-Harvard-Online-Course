bf6fe2940d42f17de87c5961ed2b806608250837
Erik Mayes <emayes528@gmail.com>
2024-08-05 01:10:00 +0000

    Mon, Aug 5, 2024, 1:10 AM +00:00

---
+++ fe/stack.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    struct Node *top;;
+}stack;
+
+void init(stack* s){
+    stack->top = 0;
+}
+int main() {
+
+}
