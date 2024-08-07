07479dc588080d8574d1ea49d3fa8889dc77c7db
Erik Mayes <emayes528@gmail.com>
2024-08-07 03:44:34 +0000

    Wed, Aug 7, 2024, 3:44 AM +00:00

--- fe/stack1.c
+++ fe/stack1.c
@@ -11,12 +11,24 @@ typedef struct stack {
     Node *top; // head pointer to stack
 }stack;
 
-int isEmpty(stack s) {
+Node *createNode(int data) {
+    Node *newNode = (Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+int isEmpty(stack *s) {
     return s->top == NULL;
 }
 
+void push(stack *s, int data) {
+    Node *newNode = createNode(data);
+}
+
 int main() {
-    stack s;
-    int a = isEmpty(s);
-    printf("The num for isEmpty is %d.\n", a);
+    stack *s;
+    printf("The num for isEmpty is %d.\n", isEmpty(s));
+    push(&s, 10);
+    push(&s, 20);
+    printf("The num for isEmpty is %d.\n", isEmpty(s));
 }
