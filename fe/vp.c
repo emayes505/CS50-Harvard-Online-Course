855a6f8000060ddc76445893c185fe511235d5a5
Erik Mayes <emayes528@gmail.com>
2024-08-11 17:59:25 +0000

    Sun, Aug 11, 2024, 5:59 PM +00:00

--- fe/vp.c
+++ fe/vp.c
@@ -7,24 +7,12 @@ typedef struct Node {
     struct Node* next;
 }Node;
 
-typedef struct {
+typedef struct map {
     char key;
     char value;
 }map;
 
-int isOpen(struct Node* head){
-    char open[] = { '{', '(', '[' };
-    char closed[] = { '}', ')', ']'};
-    for (int i = 0; i < 3; i++) {
-        if(open[i] == head->c) {
-            return 0;
-        } else if(closed[i] == head->c) {
-             return 1;
-        } else {
-             return -1;
-        }
-    }
-}
+
 int main() {
     char *s = "(){}[]";
 
