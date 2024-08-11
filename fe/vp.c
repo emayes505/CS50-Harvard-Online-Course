2b753cae8dda2eac21374632e190c853401b536c
Erik Mayes <emayes528@gmail.com>
2024-08-11 17:58:11 +0000

    Sun, Aug 11, 2024, 5:58 PM +00:00

--- fe/vp.c
+++ fe/vp.c
@@ -16,7 +16,13 @@ int isOpen(struct Node* head){
     char open[] = { '{', '(', '[' };
     char closed[] = { '}', ')', ']'};
     for (int i = 0; i < 3; i++) {
-        
+        if(open[i] == head->c) {
+            return 0;
+        } else if(closed[i] == head->c) {
+             return 1;
+        } else {
+             return -1;
+        }
     }
 }
 int main() {
