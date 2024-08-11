fcfbe3551b59bd3c2f0237617d1ec6bc984ca804
Erik Mayes <emayes528@gmail.com>
2024-08-11 16:57:12 +0000

    Sun, Aug 11, 2024, 4:57 PM +00:00

---
+++ fe/vp.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node {
+    char c;
+    struct Node* next;
+}Node;
+
+typedef struct {
+    char key;
+    char value;
+}map;
+
+int isEmpty(struct Node* head) {
+    if(head == NULL) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+Node* push (struct Node* head, char s) {
+    if(isEmpty(head) == 0) {
+        struct Node* newNode = (Node*)malloc(sizeof(struct Node*));
+        newNode->c = s;
+        newNode->next = head;
+        head = newNode;
+    return Node;
+    }
+    else {
+        return NULL;
+    }
+}
+bool isValid(char* s) {
+    map match[] = {
+        {')','('}, {']','[' }, {'}','{'}};
+    int i = 0;
+    while (s[i] != '\0') {
+        if(s[i] )
+    }
+}
