97bfae8f665db585f124f621bb0a5e3f6d0e8e64
Erik Mayes <emayes528@gmail.com>
2024-08-16 00:07:40 +0000

    Fri, Aug 16, 2024, 12:07 AM +00:00

--- fe/dm.c
+++ fe/dm.c
@@ -3,8 +3,16 @@
 
 int **triangularSum(int *base, int n) {
     int **trisum = (int**)malloc(sizeof(int*) * n);
-    w
+    int j = 0;
     for(int i = 0; i < n; i++) {
         trisum[i] = malloc(sizeof(int) * n - i);
     }
+        while(j < n) {
+        trisum[0][j] = base[j];
+        j++;
+    }
+    for(int i = 1; i < n; i++) {
+        trisum[i][]
+    }
 }
+
