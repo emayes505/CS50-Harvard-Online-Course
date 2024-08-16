34b82192fcc0056573b5d364c26e698f3fffbcda
Erik Mayes <emayes528@gmail.com>
2024-08-16 00:21:11 +0000

    Fri, Aug 16, 2024, 12:21 AM +00:00

--- fe/dm.c
+++ fe/dm.c
@@ -13,7 +13,8 @@ int **triangularSum(int *base, int n) {
         }
         else {
             for(int k = 0; k < n - i; k++) {
-            trisum[i][k] = trisum[i-1][k] + trisum[i-1][k - 1];
+                printf("%d %d\n", trisum[i-1][k], trisum[i-1][k - 1]);
+            trisum[i][k] = (trisum[i-1][k] + trisum[i-1][k - 1]);
             }
         }
     }
