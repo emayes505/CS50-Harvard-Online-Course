6d3f556d8f8f02203d04c604099d22a839066aa3
Erik Mayes <emayes528@gmail.com>
2024-08-16 00:16:36 +0000

    Fri, Aug 16, 2024, 12:16 AM +00:00

--- fe/dm.c
+++ fe/dm.c
@@ -3,16 +3,33 @@
 
 int **triangularSum(int *base, int n) {
     int **trisum = (int**)malloc(sizeof(int*) * n);
-    int j = 0;
     for(int i = 0; i < n; i++) {
         trisum[i] = malloc(sizeof(int) * n - i);
+
+        if(i = 0) {
+            for(int j = 0; j < n; j++) {
+                trisum[0][j] = base[j];
+            }
+        }
+        else {
+            for(int k = 0; k < n - i; k++) {
+            trisum[i][k] = trisum[i-1][k] + trisum[i-1][k - 1];
+            }
+        }
     }
-        while(j < n) {
-        trisum[0][j] = base[j];
-        j++;
+    return trisum;
+}
+
+int main() {
+    int base[] = {1, 2, 3, 4, 5};
+    int size = 5;
+    int **total = triangularSum(base, size);
+
+    for (int i = 0; i < size; i ++) {
+        for(int j = 0; j < size - i; j++) {
+            printf("%d", total[i][j]);
+        }
     }
-    for(int i = 1; i < n; i++) {
-        trisum[i][]
-    }
+
 }
 
