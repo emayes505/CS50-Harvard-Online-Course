48d8f8d8f4b5a07caf7bce380e248c7f06ced08a
Erik Mayes <emayes528@gmail.com>
2024-08-16 00:05:22 +0000

    Fri, Aug 16, 2024, 12:05 AM +00:00

--- fe/dm.c
+++ fe/dm.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ** triangularSum(int * base, int n) {
-    int **triSum = (int**)malloc(sizeof(int*) * n);
-
-    for (int i = 1; i < n; i++) {
-        triSum[i] = (int*)malloc(sizeof(int*) * (n - i));
-        if (i != 0) {
-        for (int k = 0; k < n - i; k++) {
-            triSum[i][k] = triSum[i-1][k] + triSum[i–1][k + 1];
-        }
-    
+int **triangularSum(int *base, int n) {
+    int **trisum = (int**)malloc(sizeof(int*) * n);
+    w
+    for(int i = 0; i < n; i++) {
+        trisum[i] = malloc(sizeof(int) * n - i);
     }
-}
-
-
-int main() {
-    int base[] = {1, 2, 3, 4, 5};
-    int n = 5;
-    triangularSum(base, n);
 }
